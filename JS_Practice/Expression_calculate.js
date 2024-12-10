97f152836131563a81b2ab702907426f2b898852
Vansh patel <vanshp9824@gmail.com>
2024-12-10 10:31:48 +0530

    js

--- JS_Practice/Expression_calculate.js
+++ JS_Practice/Expression_calculate.js
@@ -3,7 +3,7 @@ let ex=prompt("Enter any expression:");
 function cal(ex){
     console.log(ex);
     try{
-        return new Function('return' + ex)();
+        return new Function('return ' + ex)();
     }catch(error){
         return "Error";
     }
